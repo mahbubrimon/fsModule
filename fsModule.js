fe24f684f8308857a5e245fb664ceed93bb42af2
Mahbub Rimon <mahbub.wrenboy@gmail.com>
2023-01-23 00:00:58 +0600

    Update

--- fsModule.js
+++ fsModule.js
@@ -3,22 +3,33 @@ let http=require('http');
 
 let server=http.createServer(function (req,res) {
 
-    if(req.url==="/"){
-        //Asynchronous
+    if (req.url === "/") {
+        //Asynchronous File Read
         // fs.readFile('Home.html',function(error,data){
         // res.writeHead(200,{'Content-Type':'text/html'});
         // res.write(data);
         // res.end();
 
+        //Synchronous File Read
+        //     let myData=fs.readFileSync('Home.html');
+        //         res.writeHead(200,{'Content-Type':'text/html'});
+        //         res.write(myData);
+        //         res.end();
+        // }
 
-        //Synchronous
-        let myData=fs.readFileSync('Home.html');
-            res.writeHead(200,{'Content-Type':'text/html'});
-            res.write(myData);
-            res.end();
+        //Asynchronous File Write
+        fs.writeFile('demo.txt', 'May allah help me to learn MERN stack', function (err) {
+            if (err) {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write('File Write Failed');
+                res.end();
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write('File Write Successful');
+                res.end();
+            }
+        });
     }
-
 });
-
 server.listen(4040);
 console.log('Server Listen Successfully');
