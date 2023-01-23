b9ff73716be731e2556c580658815086f82da3c6
Mahbub Rimon <mahbub.wrenboy@gmail.com>
2023-01-23 11:51:06 +0600

    Update

--- fsModule.js
+++ fsModule.js
@@ -18,18 +18,95 @@ let server=http.createServer(function (req,res) {
         // }
 
         //Asynchronous File Write
-        fs.writeFile('demo.txt', 'May allah help me to learn MERN stack', function (err) {
-            if (err) {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write('File Write Failed');
-                res.end();
-            } else {
-                res.writeHead(200, {'Content-Type': 'text/html'});
-                res.write('File Write Successful');
-                res.end();
-            }
-        });
+        // fs.writeFile('demo.txt', 'May allah help me to learn MERN stack', function (err) {
+        //     if (err) {
+        //         res.writeHead(200, {'Content-Type': 'text/html'});
+        //         res.write('File Write Failed');
+        //         res.end();
+        //     } else {
+        //         res.writeHead(200, {'Content-Type': 'text/html'});
+        //         res.write('File Write Successful');
+        //         res.end();
+        //     }
+        // });
+
+        // Synchronous File Write
+        // let err=fs.writeFileSync('DemoSync.txt','Inshallah i will learn MERN.');
+        // if (err) {
+        //     res.writeHead(200, {'Content-Type': 'text/html'});
+        //     res.write('File Write Failed');
+        //     res.end();
+        //
+        // } else {
+        //     res.writeHead(200, {'Content-Type': 'text/html'});
+        //     res.write('File Write Successful');
+        //     res.end();
+        // }
+
+        // Asynchronous File Rename
+        // fs.rename('demo.txt','NewDemo.txt',function(err,){
+        //     if (err) {
+        //         res.writeHead(200, {'Content-Type': 'text/html'});
+        //         res.write('File Rename Failed');
+        //         res.end();
+        //     }else{
+        //         res.writeHead(200, {'Content-Type': 'text/html'});
+        //         res.write('File Rename Successful');
+        //         res.end();
+        //     }
+        // })
+        // Synchronous File Rename
+        // let err=fs.renameSync('NewDemo.txt','DemoNew.txt');
+        // if(err) {
+        //     res.writeHead(200, {'Content-Type': 'text/html'});
+        //     res.write('File Rename Failed');
+        //     res.end();
+        // }
+        // else{
+        //     res.writeHead(200, {'Content-Type': 'text/html'});
+        //     res.write('File Rename Successful');
+        //     res.end();
+        // }
+        // Asynchronous File Delete
+        // fs.unlink('DemoNew.txt',function(err){
+        //     if (err) {
+        //         res.writeHead(200, {'Content-Type': 'text/html'});
+        //         res.write('File Delete Failed');
+        //         res.end();
+        //     }else{
+        //         res.writeHead(200, {'Content-Type': 'text/html'});
+        //         res.write('File Delete Successful');
+        //         res.end();
+        //     }
+        // })
+        // Synchronous File Delete
+        // let err= fs.unlinkSync('demo.txt');
+        // if (err) {
+        //     res.writeHead(200, {'Content-Type': 'text/html'});
+        //     res.write('File Delete Failed');
+        //     res.end();
+        // }else{
+        //     res.writeHead(200, {'Content-Type': 'text/html'});
+        //     res.write('File Delete Successful');
+        //     res.end();
+        // }
+        // Asynchronous File Exists
+        // fs.exists('DemoSync.txt', function(get){
+        // if (get) {
+        //     res.end('true');
+        // }else{
+        //     res.end('false');
+        // }
+        // })
+        // Synchronous File Exists
+        let get =fs.existsSync('DemoSync.txt');
+        if(get){
+            res.end('true');
+        }else{
+            res.end('false');
+        }
+
     }
 });
-server.listen(4040);
+server.listen(8050);
 console.log('Server Listen Successfully');
